0d111f0b9d538a9be98489c04554c1b5754f0b9a
bilyz192 <bilyz192@gmail.com>
2017-06-04 15:34:50 +0700

    Revert "Exercise manager library"

--- oop/exercises_big/library/ListBook.java
+++
@@ -1,9 +0,0 @@
-public class ListBook {
-  public static void main(String[] args) {
-
-    // Call String.format with three integer codes.
-    String result = String.format("One: %1$d Two: %2$d Three: %3$d",
-            10, 20, 30);
-    System.out.println(result);
-  }
-}
